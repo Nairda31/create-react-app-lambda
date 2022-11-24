7edf57f0bca2874e3a8c52d70e2003c7e2607c47
Nairda31 <119001725+Nairda31@users.noreply.github.com>
2022-11-24 19:39:57 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,82 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import poza1 from './images/poza1.jpg'
+import poza2 from "./images/poza2.png";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center  h-[100%] w-screen font-bold bg-[#CD5C5C]">
+        <h1 className="text-[60px] mb-[10rem] animate-pulse">❤️Patri❤️</h1>
+        <div className="flex flex-col items-center mb-[7rem]">
+          <h1 className=" items-center mb-[3rem] ">
+            ❤️5 Motive pentru care te iubesc❤️
+          </h1>
+          <ol className="space-y-4 ml-[15px]">
+            <li>
+              ❤️Fiindca din prima zi mi-ai aratat ca esti o fata speciala.
+            </li>
+            <li>❤️Fiindca esti langa mine mereu. </li>
+            <li>❤️Fiindca cu tine nu ma plictisesc niciodata.</li>
+            <li>❤️Fiindca nu mi-as vedea viata cu altcineva.</li>
+            <li>❤️Fiindca crezi in mine.</li>
+          </ol>
+        </div>
+        <div className="flex flex-col items-center mb-[5rem] ">
+          <h1 className="mb-[10px]">❤️O poza de imi place cu noi❤️</h1>
+          <img src={poza1} alt="Nu stiu" className="h-[20rem] w-[30rem]" />
+        </div>
+        <div className="flex flex-col items-center mb-[4rem]">
+          <h1 className="mb-[2rem]">
+            ❤️Un mic rezumat la cum ne-am cunoscut❤️
+          </h1>
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            In prima zi cand te-am cunoscut nici nu ma gandeam ca o sa ajungem
+            sa avem o relatie asa buna si doar o sa mai vorbim cam 1 luna si
+            atat.Dar apoi am reusit sa vorbim sa ne vedem mai mult si TOT eu a
+            trebuie sa fac prima miscare.😅Apoi o alta parte importanta in
+            povestea noasta a mai fost cand te-am intrebat daca vrei sa fi
+            iubita mea, care din punctul meu de vedere a fost cel mai frumos
+            moment din viata mea.😇 Nu vreau sa ma lungesc prea mult fiindca mai
+            ai de citit multe chesti pe aici si vreau doar sa termin prin faptul
+            ca au fost 2 ani plini de amintiri si aventuri si , doamne ajuta, ca
+            si urmatori sa fie tot asa. 🥰🥰🥰
           </p>
-          <LambdaDemo />
-        </header>
+        </div>
+        <div className="flex flex-col items-center mb-[4rem]">
+          <h1 className="mb-[3rem]">❤️Asa te plimbi tu in capul meu❤️</h1>
+          <img src={poza2} alt="Nu stiu" className="h-[15rem] animate-spin" />
+        </div>
+
+        <div className="flex flex-col items-center mb-[4rem]">
+          <h1 className="mb-[3rem]">❤️Aproape de final❤️</h1>
+          <p>
+            ❤️Aproape am ajuns de final si vreau sa-ti spun ca esti unul dintre
+            cele mai frumoase lucruri de mi s-au intamplat.Uite un pupic ❤️:
+          </p>
+          <p className="text-[5rem]">😙</p>
+        </div>
+        <div className="flex flex-col items-center mb-[4rem]">
+          <h1 className="mb-[3rem]">❤️Semnificatie floare❤️</h1>
+          <p className="mb-[2rem]">
+            Ti-am tot luat trandafiri🌹🌹 si toti ori s-au ofilit, ori s-au
+            dezintegrat , ori cine stie ce s-a mai intamplat cu ei asa ca m-am
+            gandit sa iti iau ceva care sa tina mult mai mult.🌹🌹
+          </p>
+          <p>❤️"Te voi iubi pana cand trandafirul se va ofili"❤️</p>
+        </div>
+        <div className="flex flex-col items-center mb-[4rem]">
+          <h1 className="mb-[2rem]">❤️❤️❤️Finalul❤️❤️❤️</h1>
+          <p>
+            Chiar daca nu mereu ne intelegem, vreau sa sti ca mereu te poti baza
+            pe mine si ca te iubesc foarte mult!❤️❤️ Si orice s-ar intampla o sa
+            fiu mereu langa tine.❤️❤️
+          </p>
+        </div>
+        <h1 className="text-[2rem] animate-pulse">❤️TE IUBESC❤️</h1>
       </div>
-    )
-  }
+    </>
+  );
 }
 
-export default App
+export default App;
